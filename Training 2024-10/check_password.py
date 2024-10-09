c14503a21cd40d36a37d557d511e70684ac9b453
Peter Anema <email@peteranema.nl>
2024-10-09 10:19:02 +0200

    Update check_password.py

--- Training 2024-10/check_password.py
+++ Training 2024-10/check_password.py
@@ -3,4 +3,11 @@ filename = '10-million-password-list-top-1000000.txt'
 password = input('Give a password to check: ')
 
 with open(filename) as f:
-    known_passwords = f.readlines()
+    for line in f:
+        line = line.rstrip('\n')
+        if line == password:
+            print(f'Your password "{password}" is known. Change your password!')
+            break
+    else:
+        print('Your password is OK.')
+
